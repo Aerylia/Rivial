846c65dbb3755bc8aec6e5218978c70ca2b865c0
Arianne Meijer <Arianne Meijer>
2017-04-05 12:39:19 +0200

    Created basic file structure and generated base code

---
+++ server/src/main/java/com/applab/RivialServer.java
@@ -0,0 +1,4 @@
+package com.applab;
+// How to get the server running seperately: http://stackoverflow.com/questions/16626810/can-android-studio-be-used-to-run-standard-java-projects
+public class RivialServer {
+}
